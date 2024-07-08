8e937efa615bc14cef14b7188db539b8dd25f536
Gabriel Puga <g_puga@hotmail.com>
2024-07-08 13:31:03 -0600

    commiting DRF and Menu serializers

--- capstone-venv/littlelemon/restaurant/views.py
+++ capstone-venv/littlelemon/restaurant/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from .models import Menu
+from .serializers import MenuSerializer
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+class MenuItemsView(ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class SingleMenuItemView(RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
