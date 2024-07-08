68f4838097797ca55d88903c6a98a9ef3ffa70ba
Gabriel Puga <g_puga@hotmail.com>
2024-07-08 14:20:18 -0600

    commit change to Booking Url

--- capstone-venv/littlelemon/littlelemon/urls.py
+++ capstone-venv/littlelemon/littlelemon/urls.py
@@ -26,5 +26,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('restaurant/menu/',include('restaurant.urls')),
 #    path('restaurant/', include('restaurant.urls')),
-    path('restaurant/booking', include(router.urls)),
+    path('restaurant/booking/', include(router.urls)),
 ]
