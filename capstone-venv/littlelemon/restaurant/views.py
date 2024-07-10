972d20ebc68d7bd3b8ad8c4a5719e7e020c83c48
Gabriel Puga <g_puga@hotmail.com>
2024-07-10 12:14:05 -0600

    remove TokenObtainPairView

--- capstone-venv/littlelemon/restaurant/views.py
+++ capstone-venv/littlelemon/restaurant/views.py
@@ -4,7 +4,7 @@ from rest_framework import viewsets
 from .models import Menu, Booking
 from .serializers import MenuSerializer, BookingSerializer
 from djoser import views as djoser_views
-from rest_framework_simplejwt.views import TokenObtainPairView
+#from rest_framework_simplejwt.views import TokenObtainPairView
 #from rest_framework_jwt.views import ObtainJSONWebToken
 
 # Create your views here.
@@ -25,9 +25,9 @@ class BookingViewSet(viewsets.ModelViewSet):
 #    permission_classes = [permissions.IsAuthenticated] 
 
 # Djoser views for user registration and profile retrieval
-class UserViewSet(djoser_views.UserViewSet):
-    pass
+#class UserViewSet(djoser_views.UserViewSet):
+#    pass
 
 # Token generation (login) view
-class TokenLoginView(TokenObtainPairView):
-    pass
+#class TokenLoginView(TokenObtainPairView):
+#    pass
