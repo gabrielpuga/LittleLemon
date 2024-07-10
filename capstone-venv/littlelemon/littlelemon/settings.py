04a69f6d67432aa397b80650da435a383e0d6f67
Gabriel Puga <g_puga@hotmail.com>
2024-07-10 12:41:06 -0600

    remove Token at settings

--- capstone-venv/littlelemon/littlelemon/settings.py
+++ capstone-venv/littlelemon/littlelemon/settings.py
@@ -148,4 +148,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #add the following line
-DJOSER={"USER_ID_FIELD":"username"}
+DJOSER={
+    "USER_ID_FIELD":"username",
+    }
