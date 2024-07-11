6cc65b0dacf25787e2ca809907f25e74d380af29
Gabriel Puga <g_puga@hotmail.com>
2024-07-10 23:22:50 -0600

    commit test_model.py

--- capstone-venv/littlelemon/restaurant/models.py
+++ capstone-venv/littlelemon/restaurant/models.py
@@ -12,6 +12,7 @@ class Menu(models.Model):
     title = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     inventory = models.IntegerField()
+    #add the following method in Menu class
     def __str__(self):
-        return self.title
+        return f'{self.title} : {self.price}'
 
