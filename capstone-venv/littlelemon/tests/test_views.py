e37446425b417010152f45bbbb1c76355a714941
Gabriel Puga <g_puga@hotmail.com>
2024-07-11 09:38:19 -0600

    commit change to test_views.py

--- capstone-venv/littlelemon/tests/test_views.py
+++ capstone-venv/littlelemon/tests/test_views.py
@@ -8,26 +8,22 @@ from django.contrib.auth.models import User
 from rest_framework.authtoken.models import Token 
 
 class MenuViewTest(TestCase):
-    def setUp(self):
-        self.user = User.objects.create_user(username='testuser3', password='employee@123!')
-        
+    def setUp(self): #'setUp'(case-sensitive) is a special method used for setting up the test environment 
+        self.user = User.objects.create_user(username='testuser', password='12345')
         self.token = Token.objects.create(user=self.user)
-        
+     
         self.menu1 = Menu.objects.create(title="IceCream", price=1.00, inventory=100)
         self.menu2 = Menu.objects.create(title="Tamal", price=2.75, inventory=75)
 
     def test_getall(self):
-        
         client = APIClient()
-        
         client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
         
         response = client.get(reverse('menu-list'))  # 'menu-list' is the URL name for MenuItemsView
+        
         #print(response.content)
-
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
         expected_data = MenuSerializer([self.menu1, self.menu2], many=True).data
-
         self.assertEqual(response.data, expected_data)
         
