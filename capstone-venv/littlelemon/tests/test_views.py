f8aa63c062db856d64595cc7c2ad6f2329fe3381
Gabriel Puga <g_puga@hotmail.com>
2024-07-11 08:48:50 -0600

    commit change to test_views.py

--- capstone-venv/littlelemon/tests/test_views.py
+++ capstone-venv/littlelemon/tests/test_views.py
@@ -9,33 +9,25 @@ from rest_framework.authtoken.models import Token
 
 class MenuViewTest(TestCase):
     def setUp(self):
-        # Create a test user
         self.user = User.objects.create_user(username='testuser3', password='employee@123!')
-         # Generate a new token
+        
         self.token = Token.objects.create(user=self.user)
-        # Create test instances of Menu model
+        
         self.menu1 = Menu.objects.create(title="IceCream", price=1.00, inventory=100)
-        self.menu2 = Menu.objects.create(title="Pizza", price=10.50, inventory=50)
-        self.menu3 = Menu.objects.create(title="Burger", price=5.75, inventory=75)
+        self.menu3 = Menu.objects.create(title="Tamal", price=2.75, inventory=75)
 
     def test_getall(self):
-        # Initialize Django Rest Framework APIClient
+        
         client = APIClient()
-
-        # Set Authorization header with the token
+        
         client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+        
+        response = client.get(reverse('menu-list'))  # 'menu-list' is the URL name for MenuItemsView
+        #print(response.content)
 
-        # Make GET request to retrieve all Menu objects
-        response = client.get(reverse('menu-list'))  # Assuming 'menuitems-list' is the URL name for MenuItemsView
-        # Print response content for debugging
-        print(response.content)
-
-        # Assert that response status is 200 OK
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-        # Serialize data
         expected_data = MenuSerializer([self.menu1, self.menu2, self.menu3], many=True).data
 
-        # Assert that serialized data equals response data
         self.assertEqual(response.data, expected_data)
         
